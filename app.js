a36120a441b5b2752a2dc356cbf7c62263d4dee1
ChristianScarabelli <scachmabol@hotmail.it>
2024-11-19 14:55:46 +0100

    ADD: server init

--- app.js
+++ app.js
@@ -10,3 +10,13 @@ const posts = require('./posts.js')
 
 // rendo pubblica/accessibile al server la directory public
 app.use(express.static('public'))
+
+// creo la rotta principale del mio server
+app.get('/', (req, res) => {
+    res.send('Server del mio blog')
+})
+
+// inizializzo il mio server alla porta
+app.listen(port, () => {
+    console.log(`Il server del mio blog è in ascolto della porta: ${port}`)
+})
