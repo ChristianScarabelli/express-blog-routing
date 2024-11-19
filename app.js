ee068c495fcdc2e3f25aab04aa5f610a5a57953b
ChristianScarabelli <scachmabol@hotmail.it>
2024-11-19 15:38:04 +0100

    ADD: CRUD logic

--- app.js
+++ app.js
@@ -24,28 +24,37 @@ app.get('/posts', (req, res) => {
 })
 
 // rotta Show => visualizzare un elemento
-app.get('/posts:slug', (req, res) => {
+app.get('/posts/:slug', (req, res) => {
+    const slug = req.params.slug
+    console.log(`Ecco la pizza con slug: ${slug}`)
 
+    const post = posts.find((p) => p.slug === slug)
+
+    res.json(post)
 })
 
 // rotta Store => creare un nuovo elemento
 app.post('/posts', (req, res) => {
-
+    const slug = req.params.slug
+    res.send('Creo un nuovo post')
 })
 
 // rotta Update => modificare interamente un elemento
-app.put('/posts:slug', (req, res) => {
-
+app.put('/posts/:slug', (req, res) => {
+    const slug = req.params.slug
+    res.send(`Modifico interamente il post con slug: ${slug}`)
 })
 
 // rotta Modify => modificare parzialmente un elemento
-app.patch('/posts:slug', (req, res) => {
-
+app.patch('/posts/:slug', (req, res) => {
+    const slug = req.params.slug
+    res.send(`Modifico parzialmente il post con slug: ${slug}`)
 })
 
 // rotta Destroy => eliminare un elemento
-app.delete('/posts:slug', (req, res) => {
-
+app.delete('/posts/:slug', (req, res) => {
+    const slug = req.params.slug
+    res.send(`Elimino il post con slug: ${slug}`)
 })
 
 // inizializzo il mio server alla porta
