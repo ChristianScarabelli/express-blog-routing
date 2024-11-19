cf50c2d9376aff7a4482f431abca312e68ba48cf
ChristianScarabelli <scachmabol@hotmail.it>
2024-11-19 15:22:53 +0100

    ADD: CRUD structure

--- app.js
+++ app.js
@@ -16,6 +16,38 @@ app.get('/', (req, res) => {
     res.send('Server del mio blog')
 })
 
+// operazioni CRUD secondo convenzioni REST della risorsa posts
+
+// rotta Index => visualizzare tutti gli elementi
+app.get('/posts', (req, res) => {
+    res.json(posts)
+})
+
+// rotta Show => visualizzare un elemento
+app.get('/posts:slug', (req, res) => {
+
+})
+
+// rotta Store => creare un nuovo elemento
+app.post('/posts', (req, res) => {
+
+})
+
+// rotta Update => modificare interamente un elemento
+app.put('/posts:slug', (req, res) => {
+
+})
+
+// rotta Modify => modificare parzialmente un elemento
+app.patch('/posts:slug', (req, res) => {
+
+})
+
+// rotta Destroy => eliminare un elemento
+app.delete('/posts:slug', (req, res) => {
+
+})
+
 // inizializzo il mio server alla porta
 app.listen(port, () => {
     console.log(`Il server del mio blog è in ascolto della porta: ${port}`)
